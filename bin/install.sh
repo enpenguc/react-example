f90887f832b441703b48017b8a5edee04520a10f
enpeng <enpenguc@163.com>
2017-02-25 10:33:56 +0800

    改用dav框架开发,roadhog编译构建

---
+++ bin/install.sh
@@ -0,0 +1,3 @@
+# 安装依赖
+cnpm install --save antd babel-runtime babel-polyfill es3ify-loader dva dva-loading react react-dom classnames isomorphic-fetch echarts moment qs reselect redux-logger redux-undo
+cnpm install --save-dev babel-eslint babel-plugin-dva-hmr babel-plugin-import babel-plugin-transform-runtime eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react expect husky redbox-react roadhog pre-commit
